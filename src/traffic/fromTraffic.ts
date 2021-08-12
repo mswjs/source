aef7104408c192ec091c703582f63526349fed15
Artem Zakharchenko <kettanaito@gmail.com>
2021-08-12 13:15:05 +0200

    Adds integration tests for the "fromTraffic" function

--- src/traffic/fromTraffic.ts
+++ src/traffic/fromTraffic.ts
@@ -1,30 +1,66 @@
 import { invariant } from 'outvariant'
 import { Har, Entry } from 'har-format'
-import { RestHandler, rest } from 'msw'
+import { Cookie, parse } from 'set-cookie-parser'
+import { Headers, headersToObject } from 'headers-utils'
+import { RestHandler, rest, context, ResponseTransformer } from 'msw'
 
 function toRequestHandler(entry: Entry): RestHandler {
+  const transformers: ResponseTransformer[] = []
   const { request, response, time } = entry
   const method = request.method.toLowerCase() as keyof typeof rest
+
+  const responseHeaders: Headers = new Headers()
+  const responseCookies: Cookie[] = []
+
+  // Response status and status text.
+  transformers.push(context.status(response.status, response.statusText))
+
+  // Response headers and cookies.
+  for (const header of response.headers) {
+    if (['set-cookie', 'set-cookie2'].includes(header.name.toLowerCase())) {
+      responseCookies.push(...parse(header.value))
+      continue
+    }
+
+    responseHeaders.set(header.name, header.value)
+  }
+
+  transformers.push(context.set(headersToObject(responseHeaders)))
+
+  if (responseCookies.length > 0) {
+    responseCookies.forEach((cookie) => {
+      const { name, value, ...cookieOptions } = cookie
+      transformers.push(
+        context.cookie(name, value, {
+          ...cookieOptions,
+          sameSite: cookieOptions.sameSite === '',
+        }),
+      )
+    })
+  }
+
+  // Response time.
   const responseTime = time || 0
+  transformers.push(context.delay(responseTime))
 
-  const responseHeaders = response.headers.reduce<Record<string, string>>(
-    (headers, { name, value }) => {
-      headers[name] = value
-      return headers
-    },
-    {},
-  )
+  // Response body.
+  const { text: responseBody } = response.content
 
-  // const responseBody = response.content.text
-  //   ? Uint8Array.from(response.content.text)
-  //   : null
+  if (responseBody) {
+    console.log('MSW res body', responseBody.slice(0, 10))
+    /**
+     * @todo Convert all response bodies to Buffer.
+     * That way both buffer and non-buffer bodies can be sent.
+     */
 
-  return rest[method](request.url, (req, res, ctx) => {
-    return res(
-      ctx.status(response.status, response.statusText),
-      ctx.set(responseHeaders),
-      ctx.delay(responseTime),
-    )
+    // const buffer = Uint8Array.from(atob(responseBody), (c) => c.charCodeAt(0))
+    // console.log('MSW RES:', buffer)
+
+    transformers.push(context.body(responseBody))
+  }
+
+  return rest[method](request.url, (req, res) => {
+    return res(...transformers)
   })
 }
 
