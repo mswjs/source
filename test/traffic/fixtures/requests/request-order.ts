f8d539c7c07424cfbe00244fd671762c76dc9d72
Artem Zakharchenko <kettanaito@gmail.com>
2021-08-26 18:04:45 +0200

    fromTraffic: Support response order (#4)
    * chore: updates dependencies
    
    * feat(fromTraffic): support response order

---
+++ test/traffic/fixtures/requests/request-order.ts
@@ -0,0 +1,21 @@
+import { createTrafficScenario } from '../../utils/createTrafficScenario'
+
+createTrafficScenario(
+  (app) => {
+    let isFirstRequest = true
+
+    app.get('/resource', (req, res) => {
+      if (isFirstRequest) {
+        isFirstRequest = false
+        return res.send('one')
+      }
+
+      res.send('two')
+    })
+  },
+  (server) => [
+    // Intentionally request two same endpoints.
+    [server.http.makeUrl('/resource')],
+    [server.http.makeUrl('/resource')],
+  ],
+)
