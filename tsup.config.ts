984c7a4a56f0ac9c8850c979ad141ae75fdc7738
Artem Zakharchenko <kettanaito@gmail.com>
2024-05-08 21:24:55 +0200

    chore: emit granular esm

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,28 @@
-import { defineConfig } from 'tsup'
+import { defineConfig, Options } from 'tsup'
 
-export default defineConfig({
-  entry: ['src/index.ts'],
-  format: ['esm', 'cjs'],
-  outDir: './lib',
+const commonOptions = {
   clean: true,
+  sourcemap: true,
   tsconfig: './tsconfig.build.json',
   // dts: true,
+} satisfies Options
+
+const esm = defineConfig({
+  entry: ['src/**/*.ts'],
+  format: 'esm',
+  outDir: './lib/esm',
+  bundle: false,
+  splitting: true,
+  ...commonOptions,
 })
+
+const cjs = defineConfig({
+  entry: ['src/index.ts'],
+  format: 'cjs',
+  outDir: './lib/cjs',
+  bundle: true,
+  splitting: false,
+  ...commonOptions,
+})
+
+export default [esm, cjs]
