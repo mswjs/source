3a69b7f421753f8a81d2d0e434b003e44caf4965
Artem Zakharchenko <kettanaito@gmail.com>
2024-05-26 17:32:54 +0200

    test: fix "response-order" test

--- test/traffic/response-order.test.ts
+++ test/traffic/response-order.test.ts
@@ -1,5 +1,5 @@
 import { fromTraffic } from '../../src/fromTraffic/fromTraffic'
-import { withHandlers } from '../../test/support/withHandlers'
+import { InspectedHandler, inspectHandlers } from '../support/inspectHandler'
 import { normalizeLocalhost, readArchive } from './utils'
 
 const requestOrder = readArchive(
@@ -8,27 +8,32 @@ const requestOrder = readArchive(
 
 it('respects the response sequence when repeatedly requesting the same endpoint', async () => {
   const handlers = fromTraffic(requestOrder, normalizeLocalhost)
-  const [firstResponse, secondResponse, thirdResponse] = await withHandlers(
-    handlers,
-    async () => {
-      // Intentionally request the same endpoint.
-      return [
-        await fetch('http://localhost/resource'),
-        await fetch('http://localhost/resource'),
-        await fetch('http://localhost/resource'),
-      ]
+  expect(await inspectHandlers(handlers)).toEqual<InspectedHandler[]>([
+    // The first request handler returns a unique response.
+    {
+      handler: {
+        method: 'GET',
+        path: 'http://localhost/resource',
+      },
+      response: {
+        status: 200,
+        statusText: 'OK',
+        headers: expect.any(Array),
+        body: 'one',
+      },
     },
-  )
-
-  // The first request receives a unique response.
-  expect(firstResponse.status).toEqual(200)
-  expect(await firstResponse.text()).toEqual('one')
-
-  // The second request receives a different response.
-  expect(secondResponse.status).toEqual(200)
-  expect(await secondResponse.text()).toEqual('two')
-
-  // Any subsequent request receives the latest (second) response.
-  expect(thirdResponse.status).toEqual(200)
-  expect(await thirdResponse.text()).toEqual('two')
+    // Any subsequent request handlers produce the latest (second) response.
+    {
+      handler: {
+        method: 'GET',
+        path: 'http://localhost/resource',
+      },
+      response: {
+        status: 200,
+        statusText: 'OK',
+        headers: expect.any(Array),
+        body: 'two',
+      },
+    },
+  ])
 })
