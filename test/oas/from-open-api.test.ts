45df8944b7508871660d9e84dfe7381eed3fbca8
Jaunius Eitmantis <jaunius.eitmantis@mentimeter.com>
2024-07-17 15:51:32 +0200

    test(fromOpenApi): add test for when mapper modifies operation

--- test/oas/from-open-api.test.ts
+++ test/oas/from-open-api.test.ts
@@ -70,3 +70,104 @@ it('creates handlers based on provided filter', async () => {
     },
   ])
 })
+
+it('creates handler with modified response', async () => {
+  const openApiSpec = createOpenApiSpec({
+    paths: {
+      '/numbers': {
+        description: 'Get numbers',
+        get: {
+          responses: {
+            200: {
+              content: {
+                'application/json': {
+                  example: [1, 2, 3],
+                },
+              },
+            },
+          },
+        },
+        put: {
+          responses: {
+            200: {
+              content: {
+                'application/json': {
+                  example: [1, 2, 3],
+                },
+              },
+            },
+          },
+        },
+      },
+      '/orders': {
+        get: {
+          responses: {
+            200: {
+              content: {
+                'application/json': {
+                  example: [{ id: 1 }, { id: 2 }, { id: 3 }],
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  })
+
+  const handlers = await fromOpenApi(
+    openApiSpec,
+    ({ path, method, operation }) => {
+      return path === '/numbers' && method === 'get'
+        ? {
+            ...operation,
+            responses: {
+              200: {
+                description: 'Get numbers response',
+                content: { 'application/json': { example: [10] } },
+              },
+            },
+          }
+        : operation
+    },
+  )
+
+  expect(await inspectHandlers(handlers)).toEqual<InspectedHandler[]>([
+    {
+      handler: {
+        method: 'GET',
+        path: 'http://localhost/numbers',
+      },
+      response: {
+        status: 200,
+        statusText: 'OK',
+        headers: expect.arrayContaining([['content-type', 'application/json']]),
+        body: JSON.stringify([10]),
+      },
+    },
+    {
+      handler: {
+        method: 'PUT',
+        path: 'http://localhost/numbers',
+      },
+      response: {
+        status: 200,
+        statusText: 'OK',
+        headers: expect.arrayContaining([['content-type', 'application/json']]),
+        body: JSON.stringify([1, 2, 3]),
+      },
+    },
+    {
+      handler: {
+        method: 'GET',
+        path: 'http://localhost/orders',
+      },
+      response: {
+        status: 200,
+        statusText: 'OK',
+        headers: expect.arrayContaining([['content-type', 'application/json']]),
+        body: JSON.stringify([{ id: 1 }, { id: 2 }, { id: 3 }]),
+      },
+    },
+  ])
+})
