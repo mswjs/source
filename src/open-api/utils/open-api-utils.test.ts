bec76c0afb8377b1df9b5d8a431d8ffb6adb33e6
Kiskae <Kiskae@users.noreply.github.com>
2024-08-25 21:07:51 +0200

    fix(fromOpenApi): add disabled tests for complex accept headers

--- src/open-api/utils/open-api-utils.test.ts
+++ src/open-api/utils/open-api-utils.test.ts
@@ -25,3 +25,22 @@ it('removed empty content-types', () => {
     getAcceptedContentTypes(requestWithAccept('text/html, , */*')),
   ).toEqual(['text/html', '*/*'])
 })
+
+describe.skip('handles complex Accept headers', () => {
+  it('like weight reordering', () => {
+    expect(
+      getAcceptedContentTypes(
+        requestWithAccept(
+          'text/plain; q=0.5, text/html, text/x-dvi; q=0.8, text/x-c',
+        ),
+      ),
+    ).toEqual(['text/html', 'text/x-c', 'text/x-dvi', 'text/plain'])
+  })
+  it('like specificity reordering', () => {
+    expect(
+      getAcceptedContentTypes(
+        requestWithAccept('text/*, text/plain, text/plain;format=flowed, */*'),
+      ),
+    ).toEqual(['text/plain;format=flowed', 'text/plain', 'text/*', '*/*'])
+  })
+})
