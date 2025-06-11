f8cde73af5758de0145660a7c01224d37bf46448
Artem Zakharchenko <kettanaito@gmail.com>
2025-06-11 16:58:42 +0200

    feat: update dependencies

--- test/traffic/fixtures/requests/command.ts
+++ test/traffic/fixtures/requests/command.ts
@@ -1,5 +1,5 @@
 import { execSync } from 'node:child_process'
 
-execSync(`ts-node test/traffic/fixtures/requests/${process.argv[2]}`, {
+execSync(`tsx test/traffic/fixtures/requests/${process.argv[2]}`, {
   stdio: 'inherit',
 })
